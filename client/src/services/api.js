f4baf477a41656e4fc0cdf79e4c9ba5e6e2de908
Agam Monga <agam@Agams-MacBook-Air.local>
2026-05-22 03:21:05 +0530

    Fixed auth api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -7,4 +7,28 @@ const API = axios.create({
   },
 });
 
+export const auth = {
+  login: async (data) => {
+    const response = await API.post("/auth/login", data);
+    return response.data;
+  },
+
+  register: async (data) => {
+    const response = await API.post("/auth/register", data);
+    return response.data;
+  },
+
+  getMe: async () => {
+    const token = localStorage.getItem("crm_token");
+
+    const response = await API.get("/auth/me", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return response.data;
+  },
+};
+
 export default API;
